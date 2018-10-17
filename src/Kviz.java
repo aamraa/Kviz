306034a11c6285e748d89cfeb24ed49c5e3fe56e
Amra Mhemedovic <mehmedovic.amra@yahoo.com>
2018-10-17 17:53:37 +0200

    Primjer kVIZA

---
+++ src/Kviz.java
@@ -0,0 +1,36 @@
+
+public class Kviz {
+
+	public double zbir(double a, double b) {
+		return 0;
+	}
+
+	public double razlika(double a, double b) {
+		return 0;
+	}
+
+	public double proizvod(double a, double b) {
+		return 0;
+	}
+
+	public double vratiDijeljenje(double a, double b) {
+		if (b == 0) {
+			System.out.println("Greska.");
+			return 0;
+		} else
+			return a / b;
+
+	}
+
+	public double vratiKorijen(double a) {
+		return 0;
+	}
+
+	public double vratiKvadrat(double a) {
+		return 0;
+	}
+
+	
+}
+
+
